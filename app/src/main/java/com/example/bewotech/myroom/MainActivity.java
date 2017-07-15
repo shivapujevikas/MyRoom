5809fb0e7927ea121c1c5eb280f945bac22d80bd
GeniePlay 1 <shivapujevikas@gmail.com>
2017-07-15 17:58:45 +0530

    1st push

--- app/src/main/java/com/example/bewotech/myroom/MainActivity.java
+++ app/src/main/java/com/example/bewotech/myroom/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bewotech.myroom;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "hihih", Toast.LENGTH_SHORT).show();
 
     }
 }
